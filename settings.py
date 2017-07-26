cd31d6105ae821df3aedbfa4fed95f17a88999a8
Leo <15596688526@163.com>
2017-07-26 14:10:25 +0800

    a test

--- settings.py
+++ settings.py
@@ -23,20 +23,3 @@ for setting in dir(module):
     if setting == setting.upper():
         locals()[setting] = getattr(module, setting)
 
-TEMPLATES = [
-    {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],           # templates
-        'APP_DIRS': True,
-        'OPTIONS': {
-			'debug': DEBUG,
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
-            ],
-        },
-    },
-]
-
